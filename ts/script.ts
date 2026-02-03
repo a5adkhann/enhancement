91048ba366ca6c0a431bc30aa50de04220b8ca25
asadkaptech <163139210+asadklm@users.noreply.github.com>
2026-02-04 00:17:16 +0500

    ---

---
+++ ts/script.ts
@@ -0,0 +1,65 @@
+/*
+Basic Types
+Number, Boolean, String
+Arrays, Tuples
+Any, Unknown, Never, Void
+Enums
+*/
+
+// Array
+// let arr = [1, 2, 3, 4, "Asad"];
+
+
+// Tuple
+// let arr : [number, string] = [1, "Asad"];
+
+// let variable : any;
+
+// variable = "Asad";
+// variable = 12;
+
+// function runInfinite() : never{
+//     while(true){
+//         console.log("Infinite run");
+//     }
+// }
+
+// runInfinite();
+
+// console.log("Run After Func");
+
+// function abcd() : void {
+//     console.log("Void Func")
+// }
+
+// abcd();
+
+// enum Direction{
+//     top = "TOP",
+//     left = "LEFT",
+//     right = "RIGHT",
+//     bottom = "BOTTOM"
+// }
+
+// Direction.top;
+
+
+// enum MousePosition {
+//     x = 0,
+//     y = 0
+// }
+
+// MousePosition.x;   
+// MousePosition.y;
+
+enum Human {
+    name = "Asad",
+    age = 25
+}
+
+Human.age
+
+
+
+
+
