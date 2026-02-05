d8e109b6dec79e94697d67bccdd1865f0924a3a7
asadkaptech <163139210+asadklm@users.noreply.github.com>
2026-02-05 22:42:18 +0500

    ---

--- ts/dist/script.js
+++ ts/dist/script.js
@@ -1,3 +1,4 @@
+"use strict";
 /*
 Basic Types
 Number, Boolean, String
@@ -5,71 +6,51 @@ Arrays, Tuples
 Any, Unknown, Never, Void
 Enums
 */
-
+Object.defineProperty(exports, "__esModule", { value: true });
 // Array
 // let arr = [1, 2, 3, 4, "Asad"];
-
-
 // Tuple
 // let arr : [number, string] = [1, "Asad"];
-
 // let variable : any;
-
 // variable = "Asad";
 // variable = 12;
-
 // function runInfinite() : never{
 //     while(true){
 //         console.log("Infinite run");
 //     }
 // }
-
 // runInfinite();
-
 // console.log("Run After Func");
-
 // function abcd() : void {
 //     console.log("Void Func")
 // }
-
 // abcd();
-
 // enum Direction{
 //     top = "TOP",
 //     left = "LEFT",
 //     right = "RIGHT",
 //     bottom = "BOTTOM"
 // }
-
 // Direction.top;
-
-
 // enum MousePosition {
 //     x = 0,
 //     y = 0
 // }
-
 // MousePosition.x;   
 // MousePosition.y;
-
 // enum Human {
 //     name = "Asad",
 //     age = 25
 // }
-
 // Human.age
-
 // let variable : string | number;
-
 // variable = 12;
-
 // if (typeof variable === "string") {
 //     variable.toLowerCase();
 // }
 // else if(typeof variable === "number"){
 //     variable.toFixed();
 // }
-
 // function abcd(variable : string | number){
 //  if (typeof variable === "string") {
 //     variable.toLowerCase();
@@ -78,74 +59,40 @@ Enums
 //     variable.toFixed();
 // }   
 // }
-
 // abcd("aa");
 // abcd(12);
 // abcd(true);
-
-
 // union
 // let a: number | string;
-
 // intersection
 // let a : string & number;
-
 // type Clasmates = {
 //     section: string
 // }
-
 // type Students = {
 //     name : string
 // }
-
 // type ClassmatesInStudents = Clasmates & Students;
-
 // let a : ClassmatesInStudents = {
 //     section : "a",
 //     name : "Asad"
 // }
-
-
-
 // type City = {
 //     name : string,
 //     population : number
 // }
-
 // type Planet = {
 //     name : string,
 //     cities : number
 // }
-
 // type CitiesInPlanet = City & Planet;
-
 // let value : CitiesInPlanet = {
 //     name : "Karachi",
 //     population : 45000,
 //     cities : 12
 // }
-
-// let a : number;
-// a = 15;
-// console.log(a)
-// console.log("hey")
-
-
-// primitive type aliases
-// type Polu = string;
-// let a : Polu;
-// a="A";
-
-// object type aliases
-type Human = {
-    name : string,
-    age : number,
-    email : string
-}
-
-let a : Human = {
-    name : "Asad",
-    age : 23,
-    email : "asadklm30@gmail.com"
-}
-
+let a;
+a = 15;
+console.log(a);
+console.log("hey");
+//# sourceMappingURL=script.js.map
